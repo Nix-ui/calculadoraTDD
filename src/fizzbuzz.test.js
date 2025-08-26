d81c044930fb005ba7f0f35765d56be58dc47d69
Matheo Quiroga <matheo.quiroga@ucb.edu.bo>
2025-08-26 10:08:54 -0400

    feat: test fail for fizzbuzz

--- src/fizzbuzz.test.js
+++ src/fizzbuzz.test.js
@@ -2,6 +2,6 @@ import { fizzBuzz } from "./fizzbuzz.js";
 
 describe("FizzBuzz", () => {
     it("deberia devolver el numero si no es multiplo de 3 o 5 ",()=>{
-        expect(fizzBuzz(1)).toEqual(1);
+        expect(fizzBuzz(1)).toEqual("1");
     });
 });
