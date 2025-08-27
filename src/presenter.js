849fc89fec389eef3f7804a3b646ecebf9c75c82
Matheo Quiroga <matheo.quiroga@ucb.edu.bo>
2025-08-27 00:44:50 -0400

    feat: interfaz de index

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,14 @@
 import {sumar} from "./calculadora";
+import {generateFizzBuzzSequence, fizzBuzz} from "./fizzbuzz.js";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
+const sequence = document.querySelector("#secuence-input");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
-
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  let result = sequence.checked ? generateFizzBuzzSequence(Number(first.value)) : fizzBuzz(Number(first.value));
+  div.innerHTML = "<p>" + result + "</p>";
 });
